30161f79ace3a41fa37c15091c558d9cc5cc701b
Zainab Salih <Zainab.Salih@outlook.com>
2024-01-23 18:01:33 +0000

    TASKS 0 & 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *next, *temp, *tempPrev, *tempNext;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	current = (*list)->next;
 	while (current != NULL)
